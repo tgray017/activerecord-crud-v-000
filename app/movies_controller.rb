3c9b5d1d9c09c6b76b12a6c20384ec15f4eed2aa
tgray017 <tgray017@gmail.com>
2019-02-16 22:12:55 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -81,18 +81,18 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.all.each {|m| m.update(:title => "A Movie")}
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  movie = Movie.find_by(:title => "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.all.each {|m| m.destroy}
 end
